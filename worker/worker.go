f93164543e45522ba6f20e2748c854b72de43047
Chun-Hung Tseng <henrybear327@gmail.com>
2024-04-23 17:51:47 +0200

    Demo issue: "SomeFuncString" got value Hello_world of type "string" but expected type "string"

--- worker/worker.go
+++ worker/worker.go
@@ -3,41 +3,14 @@ package worker
 import (
 	"log"
 	"sync"
-	"time"
 )
 
-func Worker1(wg *sync.WaitGroup) {
+func WorkerString(wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	log.Println("worker1 in")
-	defer log.Println("worker1 out")
+	log.Println("WorkerString in")
+	defer log.Println("WorkerString out")
 
-	// gofail: var worker1Failpoint struct{}
-	// return
-
-	time.Sleep(3 * time.Second)
-}
-
-func Worker2(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	log.Println("worker2 in")
-	defer log.Println("worker2 out")
-
-	// gofail: var worker2Failpoint struct{}
-	// return
-
-	time.Sleep(3 * time.Second)
-}
-
-func Worker3(wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	log.Println("worker3 in")
-	defer log.Println("worker3 out")
-
-	// gofail: var worker3Failpoint struct{}
-	// return
-
-	time.Sleep(3 * time.Second)
+	// gofail: var SomeFuncString string
+	// log.Println("SomeFuncString", SomeFuncString)
 }
